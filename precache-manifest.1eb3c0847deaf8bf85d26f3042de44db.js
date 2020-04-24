7900f07b8c2a196a6a918239387ec09db52c3ee3
unknown <ayman.bmh123@gmail.com>
2020-04-24 20:26:41 +0000

    Updates

---
+++ precache-manifest.1eb3c0847deaf8bf85d26f3042de44db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde392914cf9ba7b21fac0c044bdb949",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "6d8344ae29829cbaaea9",
+    "url": "/coronavirus/static/css/main.4c8d8143.chunk.css"
+  },
+  {
+    "revision": "a24644099906e5a71a51",
+    "url": "/coronavirus/static/js/2.bfafd6bf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus/static/js/2.bfafd6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8344ae29829cbaaea9",
+    "url": "/coronavirus/static/js/main.7bdd83ae.chunk.js"
+  },
+  {
+    "revision": "e1fdf3bb4b1b462ba31e",
+    "url": "/coronavirus/static/js/runtime-main.4f63cbcc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronavirus/static/media/COVID19.d7265326.png"
+  }
+]);
