167370ccf36cc97fd35069c3bdff0ef808d26eb1
unknown <ayman.bmh123@gmail.com>
2020-04-24 21:20:02 +0000

    Updates

---
+++ precache-manifest.3be856e4d08a706d4be2f10edd10585a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aaf3652f7ebb1a841550213b34925dc",
+    "url": "/coronavirus_tracker/index.html"
+  },
+  {
+    "revision": "6d8344ae29829cbaaea9",
+    "url": "/coronavirus_tracker/static/css/main.4c8d8143.chunk.css"
+  },
+  {
+    "revision": "fe6021ae4e85b20ee504",
+    "url": "/coronavirus_tracker/static/js/2.3a51ab34.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus_tracker/static/js/2.3a51ab34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8344ae29829cbaaea9",
+    "url": "/coronavirus_tracker/static/js/main.7bdd83ae.chunk.js"
+  },
+  {
+    "revision": "c6627ee5b5277ef1175d",
+    "url": "/coronavirus_tracker/static/js/runtime-main.826d083c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronavirus_tracker/static/media/COVID19.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirus/precache-manifest.1eb3c0847deaf8bf85d26f3042de44db.js"
+  "/coronavirus_tracker/precache-manifest.3be856e4d08a706d4be2f10edd10585a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coronavirus/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/coronavirus_tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
